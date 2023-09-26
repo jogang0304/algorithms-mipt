3816fc8b57a3c2df6146ca55844ff04d69301d3b
Иван Головин <golovin.ia@phystech.edu>
2023-09-26 17:55:01 +0300

    доделал все задачи

--- Теоретическое дз 1/7.py
+++ Теоретическое дз 1/7.py
@@ -1,22 +1,48 @@
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-k = int(input())
+def f():
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    k = int(input())
 
-# max за n
-countValuesA = [0 for i in range(max(a) + 1)]
-# цикл за n
-for x in a:
-    countValuesA[x] += 1
-# max за m
-countValuesB = [0 for i in range(max(b) + 1)]
-# цикл за m
-for x in b:
-    countValuesB[x] += 1
-answer = 0
-# цикл за n
-for x in a:
-    answer += countValuesB[k - x]
+    # max за n
+    countValuesA = [0 for i in range(max(a) + 1)]
+    # цикл за n
+    for x in a:
+        countValuesA[x] += 1
+    # max за m
+    countValuesB = [0 for i in range(max(b) + 1)]
+    # цикл за m
+    for x in b:
+        countValuesB[x] += 1
+    answer = 0
+    # цикл за n
+    for x in a:
+        answer += countValuesB[k - x]
 
-print(answer)
+    print(answer)
+    # 3n + 2m = O(n + m)
 
-# 3n + 2m = O(n + m)
+
+# решение получше
+def g():
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    k = int(input())
+
+    i = 0
+    j = len(b) - 1
+    answer = 0
+    while i < len(a) and j >= 0:
+        if a[i] + b[j] == k:
+            answer += 1
+            if j > 0:
+                j -= 1
+            else:
+                i += 1
+            # нет более сложных проверок, потому что массивы строго возрастающие, то есть нет одинаковых элемментов
+        elif a[i] + b[j] < k:
+            i += 1
+        else:
+            j -= 1
+    print(answer)
+
+g()
