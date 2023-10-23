249178ccf7589d790ff9bf2469f5c5fd1386362c
Иван Головин <golovin.ia@phystech.edu>
2023-10-23 15:43:45 +0300

    исправил задачу C

--- Контест 2/c.cpp
+++ Контест 2/c.cpp
@@ -1,6 +1,18 @@
 #include <deque>
 #include <iostream>
 
+void NormalizeHalves(std::deque<int>& first_half,
+                     std::deque<int>& second_half) {
+  if (first_half.size() < second_half.size()) {
+    first_half.push_back(second_half.front());
+    second_half.pop_front();
+  }
+  if (first_half.size() > second_half.size() + 1) {
+    second_half.push_front(first_half.back());
+    first_half.pop_back();
+  }
+}
+
 int main() {
   size_t amount_of_actions;
   std::cin >> amount_of_actions;
@@ -9,25 +21,18 @@ int main() {
   for (size_t step = 0; step < amount_of_actions; ++step) {
     std::string command;
     std::cin >> command;
-    int number;
     if (command == "-") {
       std::cout << first_half.front() << std::endl;
       first_half.pop_front();
     } else {
+      int number;
       std::cin >> number;
       if (command == "+") {
         second_half.push_back(number);
-      } else {
+      } else if (command == "*") {
         first_half.push_back(number);
       }
     }
-    if (first_half.size() < second_half.size()) {
-      first_half.push_back(second_half.front());
-      second_half.pop_front();
-    }
-    if (first_half.size() > second_half.size() + 1) {
-      second_half.push_front(first_half.back());
-      first_half.pop_back();
-    }
+    NormalizeHalves(first_half, second_half);
   }
 }
