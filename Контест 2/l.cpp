4117cebd6b2117002cc2ff2abbfbfe60185817bf
Иван Головин <golovin.ia@phystech.edu>
2023-11-01 00:05:10 +0300

    сделал задачу L, не прошла кодстайл, время закончилось

--- Контест 2/l.cpp
+++ Контест 2/l.cpp
@@ -22,7 +22,11 @@ class QueueThroughStack {
     while (!stack1_.empty()) {
       stack2_.push(stack1_.top());
       if (!stack2_max_.empty()) {
-        stack2_max_.push(std::max(stack2_max_.top(), stack1_.top()));
+        if (stack2_max_.top().value >= stack1_.top().value) {
+          stack2_max_.push(stack2_max_.top());
+        } else {
+          stack2_max_.push(stack1_.top());
+        }
       } else {
         stack2_max_.push(stack1_.top());
       }
@@ -36,7 +40,12 @@ class QueueThroughStack {
   void Push(El number) {
     stack1_.push(number);
     if (!stack1_max_.empty()) {
-      stack1_max_.push(std::max(stack1_max_.top(), number));
+      El element_to_push;
+      if (stack1_max_.top().value >= number.value) {
+        stack1_max_.push(stack1_max_.top());
+      } else {
+        stack1_max_.push(number);
+      }
     } else {
       stack1_max_.push(number);
     }
@@ -53,15 +62,6 @@ class QueueThroughStack {
     stack2_max_.pop();
     return answer.value;
   }
-  int Front() {
-    if (stack2_.empty() && stack1_.empty()) {
-      return kErrorCode;
-    }
-    if (stack2_.empty()) {
-      MoveStack1ToStack2();
-    }
-    return stack2_.top().value;
-  }
   int Size() { return stack1_.size() + stack2_.size(); }
   El Max() {
     if (stack1_max_.empty() && stack2_max_.empty()) {
@@ -73,7 +73,10 @@ class QueueThroughStack {
     if (stack2_max_.empty()) {
       return stack1_max_.top();
     }
-    return std::max(stack1_max_.top(), stack2_max_.top());
+    if (stack1_max_.top().value >= stack2_max_.top().value) {
+      return stack1_max_.top();
+    }
+    return stack2_max_.top();
   }
   void Clear() {
     stack1_ = std::stack<El>();
@@ -83,80 +86,158 @@ class QueueThroughStack {
   }
 };
 
+void CountSubcolumnInner(
+    std::vector<std::vector<std::vector<El>>>& subcolumn_min,
+    QueueThroughStack& queue,
+    std::vector<std::vector<std::vector<int>>>& def_elems, int x_val, int y_val,
+    int z_val, int l_val, int y_cur, int z_cur) {
+  queue.Clear();
+  for (int x_cur = 0; x_cur < l_val - 1; ++x_cur) {
+    queue.Push({def_elems[x_cur][y_cur][z_cur], x_cur, y_cur, z_cur});
+  }
+  for (int x_cur = 0; x_cur < x_val; ++x_cur) {
+    if (queue.Size() >= l_val || x_cur > x_val - l_val) {
+      queue.Pop();
+    }
+    if (x_cur <= x_val - l_val) {
+      queue.Push({def_elems[x_cur + l_val - 1][y_cur][z_cur], x_cur + l_val - 1,
+                  y_cur, z_cur});
+    }
+    subcolumn_min[x_cur][y_cur][z_cur] = queue.Max();
+  }
+}
+void CountSubcolumn(std::vector<std::vector<std::vector<El>>>& subcolumn_min,
+                    QueueThroughStack& queue,
+                    std::vector<std::vector<std::vector<int>>>& def_elems,
+                    int x_val, int y_val, int z_val, int l_val) {
+  for (int y_cur = 0; y_cur < y_val; ++y_cur) {
+    for (int z_cur = 0; z_cur < z_val; ++z_cur) {
+      CountSubcolumnInner(subcolumn_min, queue, def_elems, x_val, y_val, z_val,
+                          l_val, y_cur, z_cur);
+    }
+  }
+}
+void CountSubmatrix(std::vector<std::vector<std::vector<El>>>& submatrix_min,
+                    std::vector<std::vector<std::vector<El>>>& subcolumn_min,
+                    QueueThroughStack& queue,
+                    std::vector<std::vector<std::vector<int>>>& def_elems,
+                    int x_val, int y_val, int z_val, int w_val) {
+  for (int x_cur = 0; x_cur < x_val; ++x_cur) {
+    for (int z_cur = 0; z_cur < z_val; ++z_cur) {
+      queue.Clear();
+      for (int y_cur = 0; y_cur < w_val - 1; ++y_cur) {
+        queue.Push(subcolumn_min[x_cur][y_cur][z_cur]);
+      }
+      for (int y_cur = 0; y_cur < y_val; ++y_cur) {
+        if (queue.Size() >= w_val || y_cur > y_val - w_val) {
+          queue.Pop();
+        }
+        if (y_cur <= y_val - w_val) {
+          queue.Push(subcolumn_min[x_cur][y_cur + w_val - 1][z_cur]);
+        }
+        submatrix_min[x_cur][y_cur][z_cur] = queue.Max();
+      }
+    }
+  }
+}
+void CountSubspace(std::vector<std::vector<std::vector<El>>>& subspace_min,
+                   std::vector<std::vector<std::vector<El>>>& submatrix_min,
+                   QueueThroughStack& queue,
+                   std::vector<std::vector<std::vector<int>>>& def_elems,
+                   int x_val, int y_val, int z_val, int h_val) {
+  for (int x_cur = 0; x_cur < x_val; ++x_cur) {
+    for (int y_cur = 0; y_cur < y_val; ++y_cur) {
+      queue.Clear();
+      for (int z_cur = 0; z_cur < h_val - 1; ++z_cur) {
+        queue.Push(submatrix_min[x_cur][y_cur][z_cur]);
+      }
+      for (int z_cur = 0; z_cur < z_val; ++z_cur) {
+        if (queue.Size() >= h_val || z_cur > z_val - h_val) {
+          queue.Pop();
+        }
+        if (z_cur <= z_val - h_val) {
+          queue.Push(submatrix_min[x_cur][y_cur][z_cur + h_val - 1]);
+        }
+        subspace_min[x_cur][y_cur][z_cur] = queue.Max();
+      }
+    }
+  }
+}
+
+std::vector<std::vector<std::vector<El>>> CountEndpoint(
+    std::vector<std::vector<std::vector<El>>>& subspace_min, int x_val,
+    int y_val, int z_val) {
+  std::vector<std::vector<std::vector<El>>> endpoints(
+      x_val, std::vector<std::vector<El>>(y_val, std::vector<El>(z_val)));
+  endpoints[x_val - 1][y_val - 1][z_val - 1] = {
+      subspace_min[x_val - 1][y_val - 1][z_val - 1].value, x_val - 1, y_val - 1,
+      z_val - 1};
+  for (int x_cur = x_val - 1; x_cur >= 0; --x_cur) {
+    for (int y_cur = y_val - 1; y_cur >= 0; --y_cur) {
+      for (int z_cur = z_val - 1; z_cur >= 0; --z_cur) {
+        El cur_subspace = subspace_min[x_cur][y_cur][z_cur];
+        if (cur_subspace.x == x_cur && cur_subspace.y == y_cur &&
+            cur_subspace.z == z_cur) {
+          endpoints[x_cur][y_cur][z_cur] = cur_subspace;
+        } else {
+          El end_place =
+              endpoints[cur_subspace.x][cur_subspace.y][cur_subspace.z];
+          endpoints[x_cur][y_cur][z_cur] = {cur_subspace.value, end_place.x,
+                                            end_place.y, end_place.z};
+        }
+      }
+    }
+  }
+  return endpoints;
+}
+
 int main() {
-  int X;
-  int Y;
-  int Z;
-  int l;
-  int w;
-  int h;
-  std::cin >> X >> Y >> Z >> l >> w >> h;
-  std::vector<std::vector<std::vector<int>>> s(
-      X, std::vector<std::vector<int>>(Y, std::vector<int>(Z, 0)));
+  int x_val;
+  int y_val;
+  int z_val;
+  int l_val;
+  int w_val;
+  int h_val;
+  std::cin >> x_val >> y_val >> z_val >> l_val >> w_val >> h_val;
+  std::vector<std::vector<std::vector<int>>> def_elems(
+      x_val, std::vector<std::vector<int>>(y_val, std::vector<int>(z_val, 0)));
+  for (int x_cur = 0; x_cur < x_val; ++x_cur) {
+    for (int y_cur = 0; y_cur < y_val; ++y_cur) {
+      for (int z_cur = 0; z_cur < z_val; ++z_cur) {
+        std::cin >> def_elems[x_cur][y_cur][z_cur];
+      }
+    }
+  }
 
   QueueThroughStack queue;
 
   // минимум на [x:x + l][y][z]
   std::vector<std::vector<std::vector<El>>> subcolumn_min(
-      X - l + 1, std::vector<std::vector<El>>(Y, std::vector<El>(Z)));
-  for (int y = 0; y < Y; ++y) {
-    for (int z = 0; z < Z; ++z) {
-      queue.Clear();
-      for (int x = 0; x < l - 1; ++x) {
-        queue.Push({s[x][y][z], x, y, z});
-      }
-      for (int x = 0; x < X - l + 1; ++x) {
-        if (queue.Size() >= l) {
-          queue.Pop();
-        }
-        queue.Push({s[x + l][y][z], x + l, y, z});
-        subcolumn_min[x][y][z] = queue.Max();
-      }
-    }
-  }
+      x_val, std::vector<std::vector<El>>(y_val, std::vector<El>(z_val)));
+  CountSubcolumn(subcolumn_min, queue, def_elems, x_val, y_val, z_val, l_val);
 
   // минимум на [x:x + l]][y:y + w][z]
   std::vector<std::vector<std::vector<El>>> submatrix_min(
-      X - l + 1, std::vector<std::vector<El>>(Y - w + 1, std::vector<El>(Z)));
-  for (int x = 0; x < X - l + 1; ++x) {
-    for (int z = 0; z < Z; ++z) {
-      queue.Clear();
-      for (int y = 0; y < w - 1; ++y) {
-        queue.Push(subcolumn_min[x][y][z]);
-      }
-      for (int y = 0; y < Y - w + 1; ++y) {
-        if (queue.Size() >= w) {
-          queue.Pop();
-        }
-        queue.Push(subcolumn_min[x][y + w][z]);
-        submatrix_min[x][y][z] = queue.Max();
-      }
-    }
-  }
+      x_val, std::vector<std::vector<El>>(y_val, std::vector<El>(z_val)));
+  CountSubmatrix(submatrix_min, subcolumn_min, queue, def_elems, x_val, y_val,
+                 z_val, w_val);
 
   // минимум на [x:x + l]][y:y + w][z:z + h]
   std::vector<std::vector<std::vector<El>>> subspace_min(
-      X - l + 1,
-      std::vector<std::vector<El>>(Y - w + 1, std::vector<El>(Z - h + 1)));
-  for (int x = 0; x < X - l + 1; ++x) {
-    for (int y = 0; y < Y - w + 1; ++y) {
-      queue.Clear();
-      for (int z = 0; z < h - 1; ++z) {
-        queue.Push(submatrix_min[x][y][z]);
-      }
-      for (int z = 0; z < Z - h + 1; ++z) {
-        if (queue.Size() >= h) {
-          queue.Pop();
-        }
-        queue.Push(submatrix_min[x][y][z + h]);
-        subspace_min[x][y][z] = queue.Max();
-      }
-    }
+      x_val, std::vector<std::vector<El>>(y_val, std::vector<El>(z_val)));
+  CountSubspace(subspace_min, submatrix_min, queue, def_elems, x_val, y_val,
+                z_val, h_val);
+
+  std::vector<std::vector<std::vector<El>>> endpoints =
+      CountEndpoint(subspace_min, x_val, y_val, z_val);
+  int n_val;
+  std::cin >> n_val;
+  for (int i = 0; i < n_val; i++) {
+    int start_x;
+    int start_y;
+    int start_z;
+    std::cin >> start_x >> start_y >> start_z;
+    El endpoint = endpoints[start_x][start_y][start_z];
+    std::cout << endpoint.x << " " << endpoint.y << " " << endpoint.z << "\n";
   }
-
-  std::vector<std::vector<std::vector<El>>> endpoints(
-      X, std::vector<std::vector<El>>(Y, std::vector<El>(Z)));
-
-  int N;
-  std::cin >> N;
 }
