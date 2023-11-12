8f2b1035868328246a4741624950ba191639990f
Иван Головин <golovin.ia@phystech.edu>
2023-11-12 12:20:50 +0300

    исправил

--- Контест 2/m.cpp
+++ Контест 2/m.cpp
@@ -16,16 +16,16 @@ void CountSums(std::vector<int64_t>& weights, std::vector<int64_t>& sums,
 }
 
 class MaxTree {
- private:
-  std::vector<int64_t> tree_;
-  int64_t array_size_;
-
  public:
   MaxTree(std::vector<int64_t>& array);
-  void Update(int64_t pos, int64_t value, int64_t tree_index = 0,
-              int64_t node_left = 0, int64_t node_right = -1);
-  int64_t GetMax(int64_t left, int64_t right, int64_t tree_index = 0,
-                 int64_t node_left = 0, int64_t node_right = -1);
+  void Update(size_t pos, int64_t value, size_t tree_index = 0,
+              size_t node_left = 0, size_t node_right = 0);
+  int64_t GetMax(size_t left, size_t right, size_t tree_index = 0,
+                 size_t node_left = 0, size_t node_right = 0) const;
+
+ private:
+  std::vector<int64_t> tree_;
+  size_t array_size_;
 };
 
 MaxTree::MaxTree(std::vector<int64_t>& array)
@@ -35,9 +35,9 @@ MaxTree::MaxTree(std::vector<int64_t>& array)
   }
 }
 
-void MaxTree::Update(int64_t pos, int64_t value, int64_t tree_index,
-                     int64_t node_left, int64_t node_right) {
-  if (node_right == -1) {
+void MaxTree::Update(size_t pos, int64_t value, size_t tree_index,
+                     size_t node_left, size_t node_right) {
+  if (node_right == 0) {
     node_right = array_size_;
   }
   if (node_left >= node_right) {
@@ -47,7 +47,7 @@ void MaxTree::Update(int64_t pos, int64_t value, int64_t tree_index,
     tree_[tree_index] = value;
     return;
   }
-  int64_t node_middle = (node_left + node_right) / 2;
+  size_t node_middle = (node_left + node_right) / 2;
   if (pos < node_middle) {
     Update(pos, value, tree_index * 2 + 1, node_left, node_middle);
   } else {
@@ -57,15 +57,15 @@ void MaxTree::Update(int64_t pos, int64_t value, int64_t tree_index,
       std::max(tree_[tree_index * 2 + 1], tree_[tree_index * 2 + 2]);
 }
 
-int64_t MaxTree::GetMax(int64_t left, int64_t right, int64_t tree_index,
-                        int64_t node_left, int64_t node_right) {
-  if (node_right == -1) {
+int64_t MaxTree::GetMax(size_t left, size_t right, size_t tree_index,
+                        size_t node_left, size_t node_right) const {
+  if (node_right == 0) {
     node_right = array_size_;
   }
   if (left == node_left && right == node_right) {
     return tree_[tree_index];
   }
-  int64_t node_middle = (node_left + node_right) / 2;
+  size_t node_middle = (node_left + node_right) / 2;
   int64_t ans = -kInf;
   if (left < node_middle) {
     ans = std::max(ans, GetMax(left, std::min(node_middle, right),
@@ -78,27 +78,16 @@ int64_t MaxTree::GetMax(int64_t left, int64_t right, int64_t tree_index,
   return ans;
 }
 
-// class MaxSparseTable {
-//  private:
-//   std::vector<std::vector<int64_t>> subarrays_;
-//   int64_t array_size_;
-//  public:
-//   MaxSparseTable(std::vector<int64_t>& array): subarrays_(0),
-//   array_size_(array.size()) {
-
-//   }
-// };
-
-int64_t GetSecondResultByFirstIndex(int64_t first_answer_index,
-                                    std::vector<int64_t>& sums,
-                                    int64_t amount_of_points,
-                                    int64_t points_for_second, MaxTree& maxs) {
+int64_t GetSecondResultByFirstIndex(size_t first_answer_index,
+                                    const std::vector<int64_t>& sums,
+                                    size_t amount_of_points,
+                                    size_t points_for_second,
+                                    const MaxTree& maxs) {
   int64_t second_answer = maxs.GetMax(
       first_answer_index + 1,
       std::min(first_answer_index + amount_of_points - points_for_second + 1,
-               static_cast<int64_t>(sums.size())));
-  if (first_answer_index + amount_of_points + 1 <
-      static_cast<int64_t>(sums.size())) {
+               sums.size()));
+  if (first_answer_index + amount_of_points + 1 < sums.size()) {
     second_answer = std::max(
         second_answer,
         maxs.GetMax(first_answer_index + amount_of_points + 1, sums.size()));
@@ -109,6 +98,33 @@ int64_t GetSecondResultByFirstIndex(int64_t first_answer_index,
                  maxs.GetMax(0, first_answer_index - points_for_second + 1));
   }
   return second_answer;
+}
+
+void CountFirstResults(std::vector<int64_t>& first_result,
+                       const std::vector<int64_t>& sums, const MaxTree& maxs,
+                       int64_t double_sum) {
+  for (size_t i = 0; i < first_result.size(); ++i) {
+    int64_t second_result = GetSecondResultByFirstIndex(
+        i, sums, first_result.size(), first_result.size() / 2, maxs);
+    first_result[i] = (double_sum / 2) - second_result;
+  }
+}
+
+std::pair<int64_t, int64_t> GetAnswers(const std::vector<int64_t>& first_result,
+                                       const std::vector<int64_t>& sums,
+                                       const MaxTree& maxs) {
+  int64_t first_answer = 0;
+  int64_t first_answer_index = 0;
+  for (size_t i = 0; i < first_result.size(); ++i) {
+    if (first_result[i] > first_answer) {
+      first_answer = first_result[i];
+      first_answer_index = i;
+    }
+  }
+  int64_t second_answer =
+      GetSecondResultByFirstIndex(first_answer_index, sums, first_result.size(),
+                                  first_result.size() / 2, maxs);
+  return {first_answer, second_answer};
 }
 
 int main() {
@@ -131,29 +147,15 @@ int main() {
   }
   int64_t points_for_second = amount_of_points / 2;
 
-  // сумма на отрезке [i:i + points_for_second)
   std::vector<int64_t> sums(amount_of_points * 2 - points_for_second);
   CountSums(weights_cycle, sums, points_for_second);
 
   MaxTree maxs(sums);
 
-  // какой счёт будет у первого игрока, если первым ходом он возьмёт вершину i
   std::vector<int64_t> first_result(amount_of_points);
-  for (int64_t i = 0; i < amount_of_points; ++i) {
-    int64_t second_result = GetSecondResultByFirstIndex(
-        i, sums, amount_of_points, points_for_second, maxs);
-    first_result[i] = (double_sum / 2) - second_result;
-  }
+  CountFirstResults(first_result, sums, maxs, double_sum);
 
-  int64_t first_answer = 0;
-  int64_t first_answer_index = 0;
-  for (int64_t i = 0; i < amount_of_points; ++i) {
-    if (first_result[i] > first_answer) {
-      first_answer = first_result[i];
-      first_answer_index = i;
-    }
-  }
-  int64_t second_answer = GetSecondResultByFirstIndex(
-      first_answer_index, sums, amount_of_points, points_for_second, maxs);
-  std::cout << first_answer << " " << second_answer;
+  auto answers = GetAnswers(first_result, sums, maxs);
+
+  std::cout << answers.first << " " << answers.second;
 }
