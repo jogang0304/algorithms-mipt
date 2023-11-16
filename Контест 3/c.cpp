a350e555046012fdfb07e0ce7260a9a5439d6afe
Иван Головин <golovin.ia@phystech.edu>
2023-11-16 17:43:13 +0300

    сделал задачу C

--- Контест 3/c.cpp
+++ Контест 3/c.cpp
@@ -1,42 +1,61 @@
-#include <algorithm>
 #include <iostream>
-#include <utility>
 #include <vector>
 
-struct TreeData {
-  size_t tree_index;
-  size_t tree_left;
-  size_t tree_right;
-};
+const int kInf = 2'000'000'000;
 
-class SortTree {
- private:
-  std::vector<std::vector<std::pair<int, int>>> tree_;
-  int a_size_;
-  static std::vector<std::pair<int, int>> Merge(
-      std::vector<std::pair<int, int>>& left,
-      std::vector<std::pair<int, int>>& right) {
-    std::vector<std::pair<int, int>> answer;
-    answer.reserve(left.size() + right.size());
-    size_t left_index = 0;
-    size_t right_index = 0;
-    while (left_index < left.size() || right_index < right.size()) {
-      if (left_index >= left.size()) {
-        answer.push_back(right[right_index]);
-        ++right_index;
-      } else if (right_index >= right.size()) {
-        answer.push_back(left[left_index]);
-        ++left_index;
-      } else if (left[left_index].first <= right[right_index].first) {
-        answer.push_back(left[left_index]);
-        ++left_index;
-      } else {
-        answer.push_back(right[right_index]);
-        ++right_index;
+class MaxTree {
+ public:
+  MaxTree(size_t size) : tree_(size * 4), array_size_(size) {}
+  MaxTree(std::vector<int>& array) : MaxTree(array.size()) { Build(array); }
+
+  void Set(size_t index, int value, size_t tree_index = 0, size_t tree_left = 0,
+           size_t tree_right = 0) {
+    if (tree_right == 0) {
+      tree_right = array_size_;
+    }
+    if (tree_left + 1 == tree_right) {
+      tree_[tree_index] = value;
+      return;
+    }
+    size_t tree_middle = (tree_left + tree_right) / 2;
+    if (index < tree_middle) {
+      Set(index, value, tree_index * 2 + 1, tree_left, tree_middle);
+    } else {
+      Set(index, value, tree_index * 2 + 2, tree_middle, tree_right);
+    }
+    tree_[tree_index] =
+        std::max(tree_[tree_index * 2 + 1], tree_[tree_index * 2 + 2]);
+  }
+
+  size_t Get(size_t index, int value, size_t tree_index = 0,
+             size_t tree_left = 0, size_t tree_right = 0) {
+    if (tree_right == 0) {
+      tree_right = array_size_;
+    }
+    if (tree_[tree_index] < value || index >= tree_right) {
+      return kInf;
+    }
+    if (tree_left + 1 == tree_right) {
+      return tree_left;
+    }
+    size_t tree_middle = (tree_left + tree_right) / 2;
+    size_t answer = kInf;
+    if (index < tree_middle) {
+      if (tree_[tree_index * 2 + 1] >= value) {
+        answer = Get(index, value, tree_index * 2 + 1, tree_left, tree_middle);
+      }
+    }
+    if (answer < index || answer == kInf) {
+      if (tree_[tree_index * 2 + 2] >= value) {
+        answer = Get(index, value, tree_index * 2 + 2, tree_middle, tree_right);
       }
     }
     return answer;
   }
+
+ private:
+  std::vector<int> tree_;
+  size_t array_size_;
 
   void Build(std::vector<int>& array, size_t tree_index = 0,
              size_t tree_left = 0, size_t tree_right = 0) {
@@ -44,102 +63,14 @@ class SortTree {
       tree_right = array.size();
     }
     if (tree_left + 1 == tree_right) {
-      tree_[tree_index].push_back(std::make_pair(array[tree_left], tree_left));
-    } else {
-      size_t tree_middle = (tree_left + tree_right) / 2;
-      Build(array, tree_index * 2 + 1, tree_left, tree_middle);
-      Build(array, tree_index * 2 + 2, tree_middle, tree_right);
-      tree_[tree_index].clear();
-      tree_[tree_index] =
-          Merge(tree_[tree_index * 2 + 1], tree_[tree_index * 2 + 2]);
+      tree_[tree_index] = array[tree_left];
+      return;
     }
-  }
-
- public:
-  SortTree(std::vector<int>& array)
-      : tree_(array.size() * 4 + 4, std::vector<std::pair<int, int>>(0)),
-        a_size_(array.size()) {
-    Build(array);
-  }
-
-  void Update(size_t index, int value, size_t tree_index = 0,
-              size_t tree_left = 0, size_t tree_right = 0) {
-    if (tree_right == 0) {
-      tree_right = a_size_;
-    }
-    if (tree_left + 1 == tree_right) {
-      tree_[tree_index].resize(1);
-      tree_[tree_index][0] = std::make_pair(value, index);
-    } else {
-      size_t tree_middle = (tree_left + tree_right) / 2;
-      if (index < tree_middle) {
-        Update(index, value, tree_index * 2 + 1, tree_left, tree_middle);
-      } else {
-        Update(index, value, tree_index * 2 + 2, tree_middle, tree_right);
-      }
-      tree_[tree_index].clear();
-      tree_[tree_index] =
-          Merge(tree_[tree_index * 2 + 1], tree_[tree_index * 2 + 2]);
-    }
-  }
-  int GetSubarray(size_t left, size_t right, int value,
-                  TreeData tree_data = {0, 0, 0}) const {
-    if (tree_data.tree_right == 0) {
-      tree_data.tree_right = a_size_;
-    }
-    if (tree_[tree_data.tree_index][tree_[tree_data.tree_index].size() - 1]
-            .first < value) {
-      return -1;
-    }
-    int answer = -1;
-    if (left == tree_data.tree_left && right == tree_data.tree_right) {
-      if (left + 1 == right) {
-        if (tree_[tree_data.tree_index][tree_[tree_data.tree_index].size() - 1]
-                .first >= value) {
-          answer = tree_[tree_data.tree_index]
-                        [tree_[tree_data.tree_index].size() - 1]
-                            .second;
-        }
-        return answer;
-      }
-
-      size_t tree_middle = (tree_data.tree_left + tree_data.tree_right) / 2;
-      auto left_answer = GetSubarray(
-          left, tree_middle, value,
-          {tree_data.tree_index * 2 + 1, tree_data.tree_left, tree_middle});
-      if (left_answer != -1) {
-        return left_answer;
-      }
-      auto right_answer = GetSubarray(
-          tree_middle, right, value,
-          {tree_data.tree_index * 2 + 2, tree_middle, tree_data.tree_right});
-      return right_answer;
-    }
-    size_t tree_middle = (tree_data.tree_left + tree_data.tree_right) / 2;
-    std::pair<std::vector<std::pair<int, int>>, int> subtree;
-    if (right <= tree_middle) {
-      answer = GetSubarray(
-          left, right, value,
-          {tree_data.tree_index * 2 + 1, tree_data.tree_left, tree_middle});
-    } else if (left >= tree_middle) {
-      answer = GetSubarray(
-          left, right, value,
-          {tree_data.tree_index * 2 + 2, tree_middle, tree_data.tree_right});
-
-    } else {
-      auto left_answer = GetSubarray(
-          left, tree_middle, value,
-          {tree_data.tree_index * 2 + 1, tree_data.tree_left, tree_middle});
-      if (left_answer != -1) {
-        answer = left_answer;
-      } else {
-        auto right_answer = GetSubarray(
-            tree_middle, right, value,
-            {tree_data.tree_index * 2 + 2, tree_middle, tree_data.tree_right});
-        answer = right_answer;
-      }
-    }
-    return answer;
+    size_t tree_middle = (tree_left + tree_right) / 2;
+    Build(array, tree_index * 2 + 1, tree_left, tree_middle);
+    Build(array, tree_index * 2 + 2, tree_middle, tree_right);
+    tree_[tree_index] =
+        std::max(tree_[tree_index * 2 + 1], tree_[tree_index * 2 + 2]);
   }
 };
 
@@ -147,24 +78,28 @@ int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(NULL);
   std::cout.tie(NULL);
-  int amount_of_elements = 0;
-  int amount_of_questions = 0;
-  std::cin >> amount_of_elements >> amount_of_questions;
-  std::vector<int> array(amount_of_elements + 1);
-  for (int i = 1; i <= amount_of_elements; ++i) {
-    std::cin >> array[i];
+  int amount_of_locations;
+  int amount_of_questions;
+  std::cin >> amount_of_locations >> amount_of_questions;
+  std::vector<int> initial_array(amount_of_locations);
+  for (int i = 0; i < amount_of_locations; ++i) {
+    std::cin >> initial_array[i];
   }
-  SortTree tree(array);
-  for (int question = 0; question < amount_of_questions; ++question) {
-    int command_number = 0;
-    int index = 0;
-    int value = 0;
-    std::cin >> command_number >> index >> value;
-    if (command_number == 0) {
-      tree.Update(index, value);
+  MaxTree tree(initial_array);
+  for (int i = 0; i < amount_of_questions; ++i) {
+    int command;
+    int index;
+    int value;
+    std::cin >> command >> index >> value;
+    if (command == 0) {
+      tree.Set(index - 1, value);
     } else {
-      auto answer = tree.GetSubarray(index, array.size(), value);
-      std::cout << answer << "\n";
+      size_t answer = tree.Get(index - 1, value);
+      if (answer != kInf) {
+        std::cout << answer + 1 << "\n";
+      } else {
+        std::cout << -1 << "\n";
+      }
     }
   }
 }
