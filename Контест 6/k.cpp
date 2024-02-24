32653e0c1d28c38b426d9c8372f7a9f580fdb097
Иван Головин <golovin.ia@phystech.edu>
2024-02-24 13:09:55 +0300

    done k

--- Контест 6/k.cpp
+++ Контест 6/k.cpp
@@ -2,17 +2,26 @@
 #include <stack>
 #include <vector>
 
-const int kInf = 1e9;
+const int kInf = __INT32_MAX__ / 2;
 
 struct Cell {
-  int max_price = -kInf;
-  int taken_position = -kInf;
+  int max_price;
+  bool element_taken;
+  Cell() : max_price(-kInf), element_taken(false){};
+  Cell(const Cell& other)
+      : max_price(other.max_price), element_taken(other.element_taken){};
+  Cell& operator=(const Cell& other) {
+    max_price = other.max_price;
+    element_taken = other.element_taken;
+    return *this;
+  }
+  ~Cell() = default;
 };
 
 int FindMaxPriceResources(const std::vector<std::vector<Cell>>& dp) {
   int answer = 0;
   for (int i = 0; i < static_cast<int>(dp[dp.size() - 1].size()); ++i) {
-    if (dp[dp.size() - 1][i].max_price > answer) {
+    if (dp[dp.size() - 1][i].max_price > dp[dp.size() - 1][answer].max_price) {
       answer = i;
     }
   }
@@ -25,11 +34,9 @@ void PrintAnswer(const std::vector<std::vector<Cell>>& dp,
   int element_position = static_cast<int>(dp.size()) - 1;
   std::stack<int> answer_stack;
   while (element_position >= 1) {
-    int taken_position =
-        dp[element_position][max_price_resources].taken_position;
-    if (taken_position != -kInf) {
-      answer_stack.push(taken_position);
-      max_price_resources -= task_resources[taken_position - 1];
+    if (dp[element_position][max_price_resources].element_taken) {
+      answer_stack.push(element_position);
+      max_price_resources -= task_resources[element_position - 1];
     }
     --element_position;
   }
@@ -67,7 +74,7 @@ Cell MaxCell(const std::vector<std::vector<Cell>>& dp,
                          [resources - task_resources[element_position - 1]]
                              .max_price +
                        task_price[element_position - 1];
-    answer.taken_position = element_position;
+    answer.element_taken = true;
   }
   return answer;
 }
@@ -79,32 +86,36 @@ struct Input {
   std::vector<int> task_price;
 };
 
-Input GetInput() {
-  Input answer;
-  std::cin >> answer.amount_of_elements >> answer.max_resources;
-  answer.task_resources.resize(answer.amount_of_elements);
-  for (int i = 0; i < static_cast<int>(answer.task_resources.size()); ++i) {
-    std::cin >> answer.task_resources[i];
+const Input* GetInput() {
+  Input* answer = new Input();
+  std::cin >> answer->amount_of_elements >> answer->max_resources;
+  answer->task_resources.resize(answer->amount_of_elements);
+  for (int i = 0; i < static_cast<int>(answer->task_resources.size()); ++i) {
+    std::cin >> answer->task_resources[i];
   }
-  answer.task_price.resize(answer.amount_of_elements);
-  for (int i = 0; i < static_cast<int>(answer.task_price.size()); ++i) {
-    std::cin >> answer.task_price[i];
+  answer->task_price.resize(answer->amount_of_elements);
+  for (int i = 0; i < static_cast<int>(answer->task_price.size()); ++i) {
+    std::cin >> answer->task_price[i];
   }
   return answer;
 }
 
 int main() {
-  Input input = GetInput();
-  std::vector<std::vector<Cell>> dp(input.amount_of_elements + 1,
-                                    std::vector<Cell>(input.max_resources + 1));
+  const Input* input = GetInput();
+  std::vector<std::vector<Cell>> dp(
+      input->amount_of_elements + 1,
+      std::vector<Cell>(input->max_resources + 1));
   for (int i = 0; i < static_cast<int>(dp[0].size()); ++i) {
     dp[0][i].max_price = 0;
   }
   for (int position = 1; position < static_cast<int>(dp.size()); ++position) {
-    for (int resources = 0; resources < static_cast<int>(dp[position].size()); ++resources) {
-      dp[position][resources] = MaxCell(dp, input.task_resources, input.task_price, position, resources);
+    for (int resources = 0; resources < static_cast<int>(dp[position].size());
+         ++resources) {
+      dp[position][resources] = MaxCell(dp, input->task_resources,
+                                        input->task_price, position, resources);
     }
   }
 
-  PrintAnswer(dp, input.task_resources);
+  PrintAnswer(dp, input->task_resources);
+  delete input;
 }
