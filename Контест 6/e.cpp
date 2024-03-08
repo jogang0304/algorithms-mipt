e4eedd7e9b15d1b04f3acd05575e5eebd141d044
Иван Головин <golovin.ia@phystech.edu>
2024-03-08 08:21:57 +0300

    e

---
+++ Контест 6/e.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+  int power;
+  std::cin >> power;
+  // dp[i][j] - how much with power i, if biggest element is j
+  std::vector<std::vector<int>> dp(power + 1, std::vector<int>(power + 1, 0));
+  std::vector<std::vector<int>> pref_sum(power + 1, std::vector<int>(power + 1, 0));
+  for (int i = 1; i <= power; ++i) {
+    for (int j = 1; j <= i; ++j) {
+      if (i == j) {
+        dp[i][j] = 1;
+        continue;
+      }
+      dp[i][j] = pref_sum[i - j][j / 2];
+    }
+    for (int j = 1; j <= power; ++j) {
+      pref_sum[i][j] = pref_sum[i][j - 1] + dp[i][j];
+    }
+  }
+  int answer = pref_sum[power][power];
+  std::cout << answer;
+}
