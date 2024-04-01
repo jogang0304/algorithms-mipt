45a8d25976691148bbd513a5fbd08e38263ecbb0
Иван Головин <golovin.ia@phystech.edu>
2024-04-01 18:16:04 +0300

    task i

---
+++ Контест 7/i.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+int main() {
+  int amount_of_languages;
+  int amount_of_programs;
+  std::cin >> amount_of_languages >> amount_of_programs;
+  
+}
