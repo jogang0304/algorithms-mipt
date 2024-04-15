689d39b701beb6519ddc80eb0a0b57944793afa3
Иван Головин <golovin.ia@phystech.edu>
2024-04-15 13:04:13 +0300

    done a

--- Контест 8/a.cpp
+++ Контест 8/a.cpp
@@ -1,72 +1,52 @@
+#include <algorithm>
 #include <iostream>
 #include <set>
 #include <vector>
 
-const int cInf = 2009000999;
+const int cInf = 2'009'000'999;
 
-struct Path {
-  int16_t destination;
-  int64_t length;
-};
-
-auto FindRoutesLengthsFromStartNode(const std::vector<std::vector<Path>>& graph,
-                                    int16_t start_node) {
-  std::vector<int64_t> ans(graph.size(), cInf);
-  ans[start_node] = 0;
-  std::set<std::pair<int64_t, int16_t>> set;
-  set.insert({ans[start_node], start_node});
-  while (!set.empty()) {
-    auto begin_node = *set.begin();
-    set.erase(set.begin());
-    for (const auto& path : graph[begin_node.second]) {
-      auto new_length = ans[begin_node.second] + path.length;
-      if (ans[path.destination] != cInf &&
-          ans[path.destination] <= new_length) {
-        continue;
+void OneBlock() {
+  int n;
+  int m;
+  std::cin >> n >> m;
+  std::vector<std::vector<std::pair<int, int>>> graph(
+      n, std::vector<std::pair<int, int>>(0));
+  for (int i = 0; i < m; ++i) {
+    int from;
+    int to;
+    int weight;
+    std::cin >> from >> to >> weight;
+    graph[from].push_back({to, weight});
+    graph[to].push_back({from, weight});
+  }
+  int start;
+  std::cin >> start;
+  std::vector<int> distances(n, cInf);
+  distances[start] = 0;
+  std::set<std::pair<int, int>> queue;
+  queue.insert({0, start});
+  while (!queue.empty()) {
+    auto b = *queue.begin();
+    queue.erase(queue.begin());
+    int v = b.second;
+    for (auto u : graph[v]) {
+      if (distances[u.first] > distances[v] + u.second) {
+        queue.erase({distances[u.first], u.first});
+        distances[u.first] = distances[v] + u.second;
+        queue.insert({distances[u.first], u.first});
       }
-      set.erase({ans[path.destination], path.destination});
-      ans[path.destination] = new_length;
-      set.insert({ans[path.destination], path.destination});
     }
   }
-  return ans;
-}
-
-auto GetGraph() {
-  int amount_of_rooms;
-  int amount_of_paths;
-  std::cin >> amount_of_rooms >> amount_of_paths;
-  std::vector<std::vector<Path>> graph(amount_of_rooms, std::vector<Path>(0));
-  for (int i = 0; i < amount_of_paths; ++i) {
-    int16_t start;
-    int16_t end;
-    int length;
-    std::cin >> start >> end >> length;
-    graph[start].push_back(Path(end, length));
-    graph[end].push_back(Path(start, length));
-  }
-  return graph;
-}
-
-void PrintResult(const std::vector<int64_t>& result) {
-  for (const auto& x : result) {
-    std::cout << x << " ";
+  for (int i = 0; i < n; ++i) {
+    std::cout << distances[i] << " ";
   }
   std::cout << "\n";
 }
 
-void ProcessOneBlock() {
-  auto graph = GetGraph();
-  int16_t start_room;
-  std::cin >> start_room;
-  auto result = FindRoutesLengthsFromStartNode(graph, start_room);
-  PrintResult(result);
-}
-
 int main() {
-  int16_t amount_of_blocks;
-  std::cin >> amount_of_blocks;
-  for (int16_t i = 0; i < amount_of_blocks; ++i) {
-    ProcessOneBlock();
+  int k;
+  std::cin >> k;
+  for (int i = 0; i < k; ++i) {
+    OneBlock();
   }
 }
